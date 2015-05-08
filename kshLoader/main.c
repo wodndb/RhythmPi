332f920a41446ab7651e90d6874235d67f4a4b85
wodndb <wodndb@koreatech.ac.kr>
2015-05-09 01:57:56 +0900

    commit from VisualStudio Code

--- kshLoader/main.c
+++ kshLoader/main.c
@@ -72,18 +72,18 @@ void getKshInfo(FILE* ksh_file_stream, KshInfo* ksh_form_info) {
 		else if(strncmp(tokAttr, "illustrator", strlen(tokAttr) + 1) == 0)	{ strcpy(ksh_form_info->illustrator, tokAttrVal); }
 		else if(strncmp(tokAttr, "difficulty", strlen(tokAttr) + 1) == 0)	{ strcpy(ksh_form_info->difficulty, tokAttrVal); }
 		else if(strncmp(tokAttr, "level", strlen(tokAttr) + 1) == 0)		{ ksh_form_info->level = atoi(tokAttrVal); }
-		else if(strncmp(tokAttr, "t", strlen(tokAttr) + 1) == 0)		{ ksh_form_info->t = atoi(tokAttrVal); }
-		else if(strncmp(tokAttr, "mvol", strlen(tokAttr) + 1) == 0)		{ ksh_form_info->mvol = atoi(tokAttrVal); }
-		else if(strncmp(tokAttr, "o", strlen(tokAttr) + 1) == 0)		{ ksh_form_info->o = atoi(tokAttrVal); }
-		else if(strncmp(tokAttr, "bg", strlen(tokAttr) + 1) == 0)		{ strcpy(ksh_form_info->bg, tokAttrVal); }
+		else if(strncmp(tokAttr, "t", strlen(tokAttr) + 1) == 0)			{ ksh_form_info->t = atoi(tokAttrVal); }
+		else if(strncmp(tokAttr, "mvol", strlen(tokAttr) + 1) == 0)			{ ksh_form_info->mvol = atoi(tokAttrVal); }
+		else if(strncmp(tokAttr, "o", strlen(tokAttr) + 1) == 0)			{ ksh_form_info->o = atoi(tokAttrVal); }
+		else if(strncmp(tokAttr, "bg", strlen(tokAttr) + 1) == 0)			{ strcpy(ksh_form_info->bg, tokAttrVal); }
 		else if(strncmp(tokAttr, "layer", strlen(tokAttr) + 1) == 0)		{ strcpy(ksh_form_info->layer, tokAttrVal); }
-		else if(strncmp(tokAttr, "po", strlen(tokAttr) + 1) == 0)		{ ksh_form_info->po = atoi(tokAttrVal); }
+		else if(strncmp(tokAttr, "po", strlen(tokAttr) + 1) == 0)			{ ksh_form_info->po = atoi(tokAttrVal); }
 		else if(strncmp(tokAttr, "plength", strlen(tokAttr) + 1) == 0)	 	{ ksh_form_info->plength = atoi(tokAttrVal); }
 		else if(strncmp(tokAttr, "pfilteragin", strlen(tokAttr) + 1) == 0)	{ ksh_form_info->pfilteragin = atoi(tokAttrVal); }
 		else if(strncmp(tokAttr, "filtertype", strlen(tokAttr) + 1) == 0)	{ strcpy(ksh_form_info->filtertype, tokAttrVal); }
 		else if(strncmp(tokAttr, "chokkakuvol", strlen(tokAttr) + 1) == 0)	{ ksh_form_info->chokkakuvol = atoi(tokAttrVal); }
-		else if(strncmp(tokAttr, "ver", strlen(tokAttr) + 1) == 0)		{ strcpy(ksh_form_info->ver, tokAttrVal); }
-		else if(strncmp(tokAttr, "m", strlen(tokAttr) + 1) == 0)		{
+		else if(strncmp(tokAttr, "ver", strlen(tokAttr) + 1) == 0)			{ strcpy(ksh_form_info->ver, tokAttrVal); }
+		else if(strncmp(tokAttr, "m", strlen(tokAttr) + 1) == 0) {
 			mIndex = 0;
 			tokAttrVal = strtok(tokAttrVal, ";");
 			while(tokAttrVal != NULL) {
@@ -92,7 +92,9 @@ void getKshInfo(FILE* ksh_file_stream, KshInfo* ksh_form_info) {
 				tokAttrVal = strtok(NULL, ";");
 			}
 		}
-		else {}
+		else {
+			//other attribute of ksh information
+		}
 
 		fgets(buffer, 115, ksh_file_stream);
 	}
@@ -136,15 +138,6 @@ int main(void) {
 	
 	getKshInfo(kshFile, ki);
 	printKshInfo(ki);
-	
-
-	/*
-	while(!feof(kshFile)) {
-		fgets(buffer, 50, kshFile);
-		printf("%s", buffer);
-		Sleep(50);
-	}
-	*/
 	
 	return 0;
 }
